e71e8125ebb7c5e2b057f856d454df99081dadfe
notTGY <microcraft47@gmail.com>
2025-12-23 23:28:35 +0300

    naive tiny stories loading

--- datamodules/tiny_stories.py
+++ datamodules/tiny_stories.py
@@ -6,6 +6,7 @@ from typing import Union
 import torch
 from torch import Tensor
 from torch.utils.data import Dataset
+from datasets import load_dataset
 
 
 class TinyStories(Dataset):
@@ -14,39 +15,21 @@ class TinyStories(Dataset):
     def __init__(
         self,
         tokenizer,
-        data_dir: Path = Path("./data"),
         block_size: int = 35,
     ) -> None:
         super().__init__()
-        self.path = data_dir / "wikitext-2.txt"
-        self.download(self.path)
-        self.tokenizer = tokenizer
-        self.data = tokenize(self.path, self.tokenizer)
+        self.ds = load_dataset("roneneldan/TinyStories", streaming=True)
+        self.dataset = list(self.ds["train"].take(len(self)))
+        
+        self.data = [tokenizer.encode(i["text"]) for i in self.dataset]
+
         self.block_size = block_size
 
     def __len__(self) -> int:
-        return len(self.data) // self.block_size - 1
+        return 100
 
     def __getitem__(self, index: int) -> tuple[Tensor, Tensor]:
-        start = index * self.block_size
-        end = start + self.block_size
-        inputs = self.data[start:end]
-        target = self.data[(start + 1) : (end + 1)]
+        data = self.data[index]
+        inputs = data[:-1]
+        target = data[1:]
         return inputs, target
-
-    @staticmethod
-    def download(destination: Path) -> None:
-        os.makedirs(destination.parent, exist_ok=True)
-        url = "https://raw.githubusercontent.com/pytorch/examples/main/word_language_model/data/wikitext-2/train.txt"
-        if os.path.exists(destination):
-            return
-        with open(destination, "w") as f:
-            f.write(requests.get(url).text)
-
-
-def tokenize(path: Path, tokenizer=None) -> Tensor:
-    assert os.path.exists(path)
-    with open(path, encoding="utf8") as f:
-        text = f.read()
-    ids = tokenizer.encode(text)
-    return torch.tensor(ids, dtype=torch.long)
