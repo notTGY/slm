03b9e4119e9658db2b672a5af176b94983d462ec
notTGY <microcraft47@gmail.com>
2025-12-23 23:29:18 +0300

    remove unused imports

--- datamodules/tiny_stories.py
+++ datamodules/tiny_stories.py
@@ -1,17 +1,9 @@
-import requests
-import os
-from pathlib import Path
-from typing import Union
-
-import torch
 from torch import Tensor
 from torch.utils.data import Dataset
 from datasets import load_dataset
 
 
 class TinyStories(Dataset):
-    """Mini version of WikiText2."""
-
     def __init__(
         self,
         tokenizer,
@@ -20,7 +12,7 @@ class TinyStories(Dataset):
         super().__init__()
         self.ds = load_dataset("roneneldan/TinyStories", streaming=True)
         self.dataset = list(self.ds["train"].take(len(self)))
-        
+
         self.data = [tokenizer.encode(i["text"]) for i in self.dataset]
 
         self.block_size = block_size
