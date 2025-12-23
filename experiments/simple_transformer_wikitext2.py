78ecb49a34ea49a2ab2cfa57ff2cd96beae8b729
notTGY <microcraft47@gmail.com>
2025-12-23 21:16:47 +0300

    log loss

--- experiments/simple_transformer_wikitext2.py
+++ experiments/simple_transformer_wikitext2.py
@@ -21,6 +21,7 @@ class LightningTransformer(LightningModule):
         inputs, target = batch
         output = self(inputs, target)
         loss = torch.nn.functional.nll_loss(output, target.view(-1))
+        self.log("train_loss", loss)
         return loss
 
     def configure_optimizers(self) -> torch.optim.Optimizer:
