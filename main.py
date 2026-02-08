d1be28a8372520a453a7ed7ead2e8846ccfd2e4e
notTGY <microcraft47@gmail.com>
2026-02-08 13:16:08 +0300

    launcher

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import os
 import argparse
+import importlib
 from difflib import SequenceMatcher
 
-from experiments.gpt_neo_tiny_stories import main
-
-parser = argparse.ArgumentParser(
-    prog='SLM experiments',
-    description='Run SLM training')
-parser.add_argument('experiment', nargs='?')
+parser = argparse.ArgumentParser(prog="SLM experiments", description="Run SLM training")
+parser.add_argument("experiment", nargs="?")
+parser.add_argument("--max_steps", default=100)
 
 base = "experiments"
 
@@ -17,25 +15,29 @@ if __name__ == "__main__":
 
     matched, module, mod_mtime, mod_match_size = False, None, 0, 0
     for f in os.listdir(base):
-        if f[-3:] != ".py": continue
-        if module is None: module = f
+        if f[-3:] != ".py":
+            continue
+        if module is None:
+            module = f
 
         if experiment is not None:
-            match = SequenceMatcher(
-                    None, f, experiment
-                ).find_longest_match(0, len(f), 0, len(experiment))
+            match = SequenceMatcher(None, f, experiment).find_longest_match(
+                0, len(f), 0, len(experiment)
+            )
             matched = matched or match.size != 0
             if match.size > mod_match_size:
                 module, mod_match_size = f, match.size
-            if matched: continue
+            if matched:
+                continue
 
-        mtime = os.stat(base+"/"+f).st_mtime
+        mtime = os.stat(base + "/" + f).st_mtime
         if mtime > mod_mtime:
             module, mod_mtime = f, mtime
     if module is None:
         print("No experiments found")
         exit(1)
 
-    print(module)
-
-    main(max_steps=100)
+    print(f"Selected: {module}")
+    imported_module = importlib.import_module(base + "." + module[:-3])
+    main = getattr(imported_module, "main")
+    main(max_steps=args.max_steps)
