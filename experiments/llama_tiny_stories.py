64dafb46fb48af566071a850b7c92255cf1212b9
notTGY <microcraft47@gmail.com>
2026-05-22 11:45:02 +0300

    hi

--- experiments/llama_tiny_stories.py
+++ experiments/llama_tiny_stories.py
@@ -151,7 +151,7 @@ def eval_model(model, tokenizer):
         print(f"Validation Perplexity: {torch.exp(val_loss).item():.2f}")
 
 
-def main(max_steps=-1, num_samples=10000, batch_size=32, seq_len=64, epochs=1):
+def main(max_steps=-1, num_samples=60000, batch_size=32, seq_len=64, epochs=1):
     os.environ["TOKENIZERS_PARALLELISM"] = "false"
     tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-125M")
     tokenizer.pad_token = tokenizer.eos_token
