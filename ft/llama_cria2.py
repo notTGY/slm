7effcea4de584d700a230a8948257bb1170ab563
notTGY <microcraft47@gmail.com>
2026-06-24 00:15:49 +0300

    2 epochs

--- ft/llama_cria2.py
+++ ft/llama_cria2.py
@@ -126,7 +126,7 @@ class LightningTransformer(LightningModule):
         }
 
 
-def main(max_steps=-1, num_samples=23941, batch_size=32, max_length=65, epochs=1):
+def main(max_steps=-1, num_samples=23941, batch_size=32, max_length=65, epochs=2):
     os.environ["TOKENIZERS_PARALLELISM"] = "false"
     tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-125M")
     tokenizer.pad_token_id = tokenizer.eos_token_id
